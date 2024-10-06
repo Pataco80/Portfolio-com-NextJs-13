2db3ea023c5c2c5631e2866f2bc51c0237277211
Ricardo Do Vale <pataco80@gmail.com>
2024-10-06 17:31:51 +0200

    Project list in Project page

---
+++ app/components/pages/projects/ProjectCard/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+const image = ''
+type ProjectCardProps = {
+  name: string,
+  url: string,
+  image: string,
+  altImage: string,
+  description: string,
+  techs:string[],
+  githubUrl?: string,
+  liveUrl?:string
+}
+
+export const ProjectCard = ({ name, url, image, altImage, description, techs }: ProjectCardProps) => {
+  return (
+    <Link href={url}>
+      <article className='group h-[450px] flex flex-col bg-pale-sky-800 border-2 border-pale-sky-800 transition-all hover:border-blue-ribbon opacity-70 hover:opacity-100 rounded-lg overflow-hidden'>
+      <div className='w-full h-48 overflow-hidden'>
+        <Image className='w-full h-full object-cover group-hover:scale-110 duration-500 transition-all' src={`/images/projects/${image}`} alt={altImage} width={380} height={200} />
+      </div>
+      <div className='flex-1 flex flex-col p-8'>
+        <strong className='font-medium text-pale-sky-50/90 group-hover:text-blue-ribbon transition-all'>{name}</strong>
+        <p className='my-2 text-pale-sky-400 line-clamp-4'>{description}</p>
+        <p className='text-pale-sky-300 text-sm block truncate'>{techs.join(", ")}</p>
+      </div>
+      </article>
+    </Link>
+  )
+}
--- app/components/pages/projects/ProjectsList/index.tsx
+++ app/components/pages/projects/ProjectsList/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
-
+import { ProjectCard } from '@/app/components/pages/projects/ProjectCard'
+import data from '@/app/data/projects-list.json'
 export const ProjectsList = () => {
+  const projectsList = data.projects_list
   return (
-    <section>ProjectsList</section>
+    <section className='container py-32 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-x-4 gap-y-6'>
+      {projectsList.map((item, index) => {
+        const { name, url, image, altImage, description, techs } = item
+        return <ProjectCard key={index} name={name} url={url} image={image} altImage={altImage} description={description} techs={techs} />
+      })}
+    </section>
   )
 }
