18d14a463b80effb458c86dfe3b797039c4224c0
Ricardo Do Vale <pataco80@gmail.com>
2024-10-07 16:00:14 +0200

    project page

---
+++ app/components/pages/projects/ProjectDetails/index.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@/app/components/Button'
+import { Link } from '@/app/components/Link';
+import { SectionTitle } from '@/app/components/SectionTitle'
+import { TechBadge } from '@/app/components/TechBadge'
+import React from 'react'
+import { HiArrowNarrowLeft } from 'react-icons/hi';
+import { TbBrandGithub, TbGlobe } from 'react-icons/tb';
+
+
+
+export const ProjectDetails = () => {
+  return (
+    <section className='w-full sm:min-h-[750px] relative flex flex-col items-center justify-end pb-10 sm:pb-24 px-6 py-24'>
+      <div className='absolute inset-0 z-[-1]' style={{ background: 'url(/images/bg-portfolio-hero.webp) no-repeat right bottom/cover, url(/images/projects/default-1.jpg) no-repeat center/cover' }} />
+      <SectionTitle title='My project'subtitle='projet' className='text-center items-center sm:[&>h3]:text-4xl' />
+      <p className='text-pale-sky-400 text-center max-w-[640px] my-4 sm:my-6 text-sm sm:text-base'>BookWise é uma plataforma de avaliação de livros que foi desenvolvida durante o bootcamp Ignite da Rocketseat. Com apenas um Figma precisávamos desenvolver essa aplicação completa Full Stack com Next.js.</p>
+      <div className='w-full sm:max-w-[330px] flex items-center justify-center gap-2 flex-wrap'>
+        <TechBadge name='Nextjs'/>
+        <TechBadge name='Nextjs'/>
+        <TechBadge name='Nextjs'/>
+        <TechBadge name='Nextjs'/>
+        <TechBadge name='Nextjs'/>
+      </div>
+      <div className='w-full my-6 sm:my-12 flex flex-col sm:flex-row items-center justify-center gap-4'>
+        <a href="#" target='_blank'>
+          <Button className='min-w-[180px]'>
+            <TbBrandGithub size={20}/>Link github</Button>
+        </a>
+        <a href="#" target='_blank'>
+          <Button className='min-w-[180px]'>
+            <TbGlobe size={20}/>Link github</Button>
+        </a>
+      </div>
+      <Link href="/projects">
+      <HiArrowNarrowLeft size={20}/>Voir tous les projets</Link>
+    </section>
+  )
+}
---
+++ app/components/pages/projects/ProjectSection/index.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+import React from 'react'
+
+const sections = [
+  {
+					"title":"Home Page BookWise",
+					"image":"default-1.jpg"
+				},
+				{
+					"title":"Login BookWise",
+					"image":"default-2.jpeg"
+				}
+]
+
+export const ProjectSection = () => {
+  return (
+    <section className='container my-12 md:my-32 flex flex-col gap-8 md:gap-32'>
+      {
+        sections.map(section => {
+          return (
+            <div key={section.title} className="flex flex-col items-center gap-6 md:gap-12">
+              <h2 className="text-2xl md:text-3xl font-medium text-pale-sky-300">{section.title}</h2>
+              <Image src={`/images/projects/${section.image}`} alt={`image de ${section.title}`} width={1080} height={672} className='w-full aspect-auto rounded-lg object-cover' unoptimized/>
+            </div>
+          )
+        })
+      }
+    </section>
+  )
+}
