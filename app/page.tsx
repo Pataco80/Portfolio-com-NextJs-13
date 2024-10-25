2920a9bbb46a211e5b86448bee106bf9a9bd40e1
Ricardo Do Vale <pataco80@gmail.com>
2024-10-25 15:58:00 +0200

    add query home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,33 @@ import { HeroSection } from '@/app/components/pages/home/HeroSection'
 import { KnowTechs } from '@/app/components/pages/home/KnowTechs'
 import { HighlightedProjects } from '@/app/components/pages/home/HighlightedProjects'
 import { WorkExperience } from '@/app/components/pages/home/WorkExperience'
+import { fetchHygraphQuery } from '@/app/utils/fetch-hygraph-query'
+
+const getPageData = async () => {
+	const query = `
+		query homePageData {
+  		pages(where: {slug: "home"}) {
+    		introduction {
+      		raw
+    		}
+    		knownTechs {
+      		iconSvg
+      		name
+      		startDate
+    		}
+    		technologies {
+      		name
+    		}
+  		}
+		}
+	`
+
+	return fetchHygraphQuery(query)
+}
+
 export default async function Home() {
+	const response = await getPageData()
+	console.log(response)
 	return (
 		<>
 			<HeroSection />
