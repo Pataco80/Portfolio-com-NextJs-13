d45d344b710f8d9ac003ab974e5c800d77fafa76
Pataco80 <pataco80@gmail.com>
2026-06-20 22:09:36 +0200

    style(divider): degrade gris->bleu->gris par defaut

--- app/components/DividerLine.tsx
+++ app/components/DividerLine.tsx
@@ -11,6 +11,6 @@ export const DividerHorizontal = ({ className }: DividerLineProps) => {
 
 	// JSX Component
 	return (
-		<div className={cn('w-full h-[2px] my-6 bg-pale-sky-600', className)} />
+		<div className={cn('w-full h-[2px] my-6 bg-gradient-to-r from-pale-sky-700 via-blue-ribbon-500/60 to-pale-sky-700', className)} />
 	)
 }
